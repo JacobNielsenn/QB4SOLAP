a7438fd06486bc91e14777776dcd634631996557
LAPTOP-DCTSJH6C\Jacob <janiel13@student.aau.dk>
2016-12-01 15:14:20 +0100

    Minor fixed for the presentation tomorrow.

--- EXTBIwebsite/SOLAPTool/scripts/runQuery.js
+++ EXTBIwebsite/SOLAPTool/scripts/runQuery.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
     $('#QueryButton').click( function () {
         var ele = document.getElementById('GeneratedQuery');
         var actualquery = prefixes + "\n" + ele.value;
+        console.log(actualquery);
         $.post('http://localhost:8890/sparql', {query: actualquery, format:'text/html'}, function(data) {
             console.log(data);
             $('#ResultFromQuery').html(data);
