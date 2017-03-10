972c1fcac90a7506a1bd64d6dbc400d6a02e36fa
DESKTOP-EDEQOLA\JN <janiel13@student.aau.dk>
2017-03-10 23:15:36 +0100

    Map Fixed

--- EXTBIwebsite/SOLAPTool/scripts/map.js
+++ EXTBIwebsite/SOLAPTool/scripts/map.js
@@ -14,7 +14,7 @@ function onMapClick(e) {
 function initializeMap(mapID){
     var mymap = L.map(mapID).setView([57.04745, 9.91928], 3);
     maps.push([mymap, mapID]);
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
         '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
