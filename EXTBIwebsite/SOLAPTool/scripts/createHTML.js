18e3c3683bb6c8ff925462a13a576636a2191ff7
JacobNielsenn <janiel13@student.aau.dk>
2017-04-11 10:08:05 +0200

    Ready for release.

--- EXTBIwebsite/SOLAPTool/scripts/createHTML.js
+++ EXTBIwebsite/SOLAPTool/scripts/createHTML.js
@@ -218,7 +218,7 @@ function InsertMultiMenuLevel(objectArrays, width, updater, field){
     }
     return menu;
 }
-//header HTML components
+//header and button HTML components
 function Insertheader(name){
     var header = InsertDiv();
     var title = InsertDiv();
@@ -497,6 +497,7 @@ function MoveUp(element){
         Q.printQuery();
     }
 }
+//Used by buttons to move the elements
 function swapElements(obj1, obj2) {
     // save the location of obj2
     if(obj2.constructor != HTMLParagraphElement){
